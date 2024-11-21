340d7f05465699c8ffd9dddc72112b1d94d438ca
Odumosu Matthew <odumosumatthew9@gmail.com>
2024-11-21 10:32:55 +0100

    Challenge 2 implementation

---
+++ Challenge2/DuplicateIdentifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2
+{
+    public class DuplicateIdentifier<T> where T : IEquatable<T>
+    {
+        public static void IdentifyDuplicates(List<T> collectionA, List<T> collectionS)
+        {
+            HashSet<T> setA = new HashSet<T>(collectionA);
+
+            foreach (var element in collectionS)
+            {
+                bool isPresent = setA.Contains(element);
+                Console.WriteLine($"{element}:{isPresent.ToString().ToLower()}");
+            }
+        }
+    }
+}
